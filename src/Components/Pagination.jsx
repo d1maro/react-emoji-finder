3e02a72022703e7e3351d16fdf2ae5247455f0e0
student1 <dima.romakhov@gmail.com>
2022-10-21 15:04:40 +0300

    button with last page fixed

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -7,21 +7,25 @@ import React from 'react'
 
 export const Pagination = ({lastIndex, setCurrentPage, getPerPage, currentPage}) => {
     let arr = [];
-    for (let i = 1; i < lastIndex; i++) {
+    for (let i = 1; i <= lastIndex; i++) {
         arr.push(i)
     }
     return (
     <footer className="footer">
-        <div className="container">
-            <button className="pagination__button" onClick={() => setCurrentPage(currentPage = 1)}>First</button>
-            {arr.map(elem => <button className="pagination__button" onClick={() => setCurrentPage(elem)}>{elem}</button>)}
-            <button className="pagination__button" onClick={() => setCurrentPage({currentPage} = -1)}>Last</button>
+        <div className="container pagination">
+            <div className="pagination__pages">
+                <button className="pagination__button" onClick={() => setCurrentPage(currentPage = 1)}>First</button>
+                {arr.map(elem => <button className="pagination__button" onClick={() => setCurrentPage(elem)}>{elem}</button>)}
+                <button className="pagination__button" onClick={() => setCurrentPage(lastIndex)}>Last</button>
+            </div>
+            <div className="pagination__select">
             <label for="select">Per Page</label>
                 <select id="select" className="pagination__button" onChange={getPerPage}>
                     <option value={12}>12</option>
                     <option value={24}>24</option>
                     <option value={48}>48</option>
-                </select>  
+                </select> 
+            </div>
         </div>
     </footer>
   )
